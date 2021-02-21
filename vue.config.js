ab646819f2eadf1239f1e4858a9fd82a29ad04b7
Evert Trollip <evert.trollip@careerteam.de>
2021-02-21 14:29:45 +0200

    updates webpack

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,12 @@ module.exports = {
   chainWebpack: config => {
     config.externals(['vue', 'vuetify']);
   },
+  configureWebpack: {
+    output: {
+      filename: `[name].js`,
+      chunkFilename: `[name].js`,
+    },
+  },
   transpileDependencies: [
     'vuetify'
   ]
